e1063d135b4b2945dbab81c3bb4c227c04669a2b
Asus <Bichk_di78@student.itstep.org>
2023-05-04 16:06:10 +0300

    Lesson_4

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
+
 import time
-print("Загрузка...")
-def time_second(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        time.sleep(1.7)
-        result = func(*args, **kwargs)
-        finish = time.time()
-        print(f"Название функцыи: {func.__name__}  Время: {round(finish - start , 2)} секунд ")
-        return result
+def delay_decorator(func):
+    def wrapper(*args , **kwargs):
+        rezult = func(*args , **kwargs)
+        for i in range(3):
+         time.sleep(1.5)
+         print(rezult)
+        return rezult
     return wrapper
-@time_second
-def lesson_4(number):
-    if number < 2:
-        return False
-    for i in range(2, int(number**0.5)+1):
-        if number % i == 0:
-            return False
-    return True
-print("Проверка на простое число:", lesson_4(56))
+
+@delay_decorator
+def sleepy(fuction):
+    return fuction
+sleepy('Пошли гулять')
